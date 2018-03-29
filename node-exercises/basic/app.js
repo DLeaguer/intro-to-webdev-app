4e5d5148442fb08d172e5933946eb785f28fcf20
DLeaguer <immostlikely@hotmail.com>
2018-03-29 00:21:35 -1000

    added comments to basic.app.js

--- node-exercises/basic/app.js
+++ node-exercises/basic/app.js
@@ -1,6 +1,12 @@
+//node fetch http from library
 var http = require('http');
+//http create web server, function (request, response)
 http.createServer(function (req, res) {
+  //status code successful, let browser know content type is text
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  //end request give back response to user 'Hello World'
   res.end('Hello World\n');
+  //listen on port 8080, 127.0.0.1 refers to yourself localhost
 }).listen(8080, '127.0.0.1');
+//write to git bash shell
 console.log('Server running at http://127.0.0.1:8080/');
